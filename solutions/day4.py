8371bf83f98b153022f050f4b2fa557396189cbd
jacty <saej3101@usherbrooke.ca>
2025-12-04 01:10:25 -0500

    Add day 4 two stars solutio real onen

--- solutions/day4.py
+++ solutions/day4.py
@@ -14,21 +14,32 @@ def main():
 
     adjacents_points = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
 
-    valid_rolls = 0
+    invalid_rolls = 0
+    old_invalid_rolls = -1
+    while invalid_rolls != old_invalid_rolls:
+        old_invalid_rolls = invalid_rolls
+        for i in range(len(rolls_diagram)):
+            for j in range(len(rolls_diagram[i])):
+                adjacent_count = 0
+
+                if rolls_diagram[i][j] == '@':
+                    for adj in adjacents_points:
+                        adj_i = i + adj[0]
+                        adj_j = j + adj[1]
+                        if 0 <= adj_i < len(rolls_diagram) and 0 <= adj_j < len(rolls_diagram[i]):
+                            if rolls_diagram[adj_i][adj_j] == '@':
+                                adjacent_count += 1
+
+                    if adjacent_count < 4:
+                        rolls_diagram[i][j] = '.'
+                        invalid_rolls += 1
+
+
+    print("Final rolls diagram:")
     for i in range(len(rolls_diagram)):
         for j in range(len(rolls_diagram[i])):
-            adjacent_count = 0
+            print(rolls_diagram[i][j], end='')
+        print()
 
-            if rolls_diagram[i][j] == '@':
-                for adj in adjacents_points:
-                    adj_i = i + adj[0]
-                    adj_j = j + adj[1]
-                    if 0 <= adj_i < len(rolls_diagram) and 0 <= adj_j < len(rolls_diagram[i]):
-                        if rolls_diagram[adj_i][adj_j] == '@':
-                            adjacent_count += 1
-
-                if adjacent_count < 4:
-                    valid_rolls += 1
-
-    print(f"valid_rolls : {valid_rolls}")
+    print(f"invalid_rolls : {invalid_rolls}")
 main()
